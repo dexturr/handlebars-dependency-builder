c5a05dd7c10697d83e4703bbb3567884e766dcd5
Dexter Edwards <dexter.edwards@ig.com>
2018-11-19 13:34:26 +0000

    Make it sync for simplicity

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ module.exports = class HandblebarsDependencyBuilder {
     this.componentsRegistry = {};
   }
 
-  async getFlattenedDependencies(pathName) {
-    const files = await globby(pathName, { absolute: true })
+  getFlattenedDependencies(pathName) {
+    const files = globby.sync(pathName, { absolute: true })
     if (files.length < 1) {
       throw new Error('No files found');
     }
     const componentNames = files.map((file) => file.split('components/')[1].replace('.hbs', ''));
     componentNames.map((name) => this.componentsRegistry[name] = []);
     for (const file of files) {
-      const fileText = await this.readFile(file);
+      const fileText = fs.readFileSync(file, { encoding: 'UTF-8'});
       this.applyTransform(this.findDeps.bind(this), file, fileText)
     }
     let resolvedDeps = [];
@@ -60,9 +60,9 @@ module.exports = class HandblebarsDependencyBuilder {
     }
   
     if(!this.componentsRegistry[invokedComponentName]) {
-      console.log(`Could not find component ${invokedComponentName}`)
+      console.log(`Could not find component ${invokedComponentName}`);
     } else if(!this.componentsRegistry[componentName].includes(invokedComponentName)) {
-      this.componentsRegistry[componentName].push(invokedComponentName)
+      this.componentsRegistry[componentName].push(invokedComponentName);
     }
   }
 
@@ -87,13 +87,6 @@ module.exports = class HandblebarsDependencyBuilder {
     });
   }
 
-  readFile(filePath) {
-    return new Promise((resolve, reject) => {
-      fs.readFile(filePath, 'utf8', (err, contents) => {
-        err ? reject(err) : resolve(contents);
-      });
-    });
-  }
 
   applyTransform(plugin, filePath, contents) {
     const code = plugin(
--- tests/tests.js
+++ tests/tests.js
@@ -3,18 +3,15 @@ const assert = require('assert');
 const DepBuilder = require('../index');
 
 describe('app', function() {
-  it('should do the thing', function(done) {
+  it('should do the thing', function() {
     const pathName = path.resolve(__dirname, 'components/**/*.hbs');
     const depBuilder = new DepBuilder();
-    depBuilder.getFlattenedDependencies(pathName)
-      .then((deps) => {
-        assert.deepStrictEqual(deps['test'], []);
-        assert.deepStrictEqual(deps['test2'], [ 'test' ]);
-        assert.deepStrictEqual(deps['test3'], [ 'test2', 'nested/nested', 'test' ]);
-        assert.deepStrictEqual(deps['test4'], [ 'test3', 'test2', 'nested/nested', 'test' ]);
-        assert.deepStrictEqual(deps['nested/nested'], [ 'test' ]);
-        done();
-      });
+    const deps = depBuilder.getFlattenedDependencies(pathName)
+    assert.deepStrictEqual(deps['test'], []);
+    assert.deepStrictEqual(deps['test2'], [ 'test' ]);
+    assert.deepStrictEqual(deps['test3'], [ 'test2', 'nested/nested', 'test' ]);
+    assert.deepStrictEqual(deps['test4'], [ 'test3', 'test2', 'nested/nested', 'test' ]);
+    assert.deepStrictEqual(deps['nested/nested'], [ 'test' ]);
   });
 });
 
