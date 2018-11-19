e853897da457b0387bfcbb8d655c8ca80ed7d522
Dexter Edwards <dexter.edwards@ig.com>
2018-11-19 11:29:18 +0000

    Add eslint and do a little refactoring

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "amd": true,
+      },
+    "extends": ["eslint:recommended", "plugin:node/recommended"],
+    "rules": {
+      "indent": ["error", 2]
+    }
+};
--- tests/tests.js
+++ tests/tests.js
@@ -9,44 +9,44 @@ const builtInHelpers = ['if', 'unless', 'each', 'with', 'log', 'lookup', 'let'];
 let componentsReigistry = {};
 
 const pushOrIgnore = (array ,itemsToPush) => {
-    itemsToPush.forEach((item) => {
-        if (!array.includes(item)) {
-            array.push(item);
-        }
-    });
+  itemsToPush.forEach((item) => {
+    if (!array.includes(item)) {
+      array.push(item);
+    }
+  });
 }
 
 const pushToDepsArray = (invokedComponentName, componentName) => {
-    if(!componentsReigistry[invokedComponentName]) {
-        console.log(`Could not find component ${invokedComponentName} on registry`)
-      } else {
-          if(!componentsReigistry[componentName].includes(invokedComponentName) 
-            && !builtInHelpers.includes(invokedComponentName)) {
-            componentsReigistry[componentName].push(invokedComponentName)
-          }
-      }
+  if(builtInHelpers.includes(invokedComponentName)) {
+    return;
+  }
+
+  if(!componentsReigistry[invokedComponentName]) {
+    console.log(`Could not find component ${invokedComponentName}`)
+  } else if(!componentsReigistry[componentName].includes(invokedComponentName)) {
+    componentsReigistry[componentName].push(invokedComponentName)
+  }
 }
 
 const findDeps = function({ source, path }, { parse, visit }) {
-    const ast = parse(source);
+  const ast = parse(source);
   
-    return visit(ast, env => {
-      let { builders: b } = env.syntax;
-      const componentName = path.split('components/')[1].replace('.hbs', '');
-      return {
-        MustacheStatement(node) {
-          const invokedComponentName = node.path.original;
-          pushToDepsArray(invokedComponentName, componentName);
-          return node;
-        },
-        BlockStatement(node) {
-          const invokedComponentName = node.path.original;
-          pushToDepsArray(invokedComponentName, componentName);
-          return node;
-        }
-      };
-    });
-  };
+  return visit(ast, () => {
+    const componentName = path.split('components/')[1].replace('.hbs', '');
+    return {
+      MustacheStatement(node) {
+        const invokedComponentName = node.path.original;
+        pushToDepsArray(invokedComponentName, componentName);
+        return node;
+      },
+      BlockStatement(node) {
+        const invokedComponentName = node.path.original;
+        pushToDepsArray(invokedComponentName, componentName);
+        return node;
+      }
+    };
+  });
+};
 
 function readFile(filePath) {
   return new Promise((resolve, reject) => {
@@ -57,74 +57,77 @@ function readFile(filePath) {
 }
 
 function applyTransform(plugin, filePath, contents) {
-    const code = plugin(
-      {
-        path: filePath,
-        source: contents,
+  const code = plugin(
+    {
+      path: filePath,
+      source: contents,
+    },
+    {
+      parse,
+      visit(ast, callback) {
+        const results = transform(ast, callback);
+        return results && results.code;
       },
-      {
-        parse,
-        visit(ast, callback) {
-          const results = transform(ast, callback);
-          return results && results.code;
-        },
-      }
-    );
+    }
+  );
   
-    return {
-      skipped: !code,
-      changed: code !== contents,
-      source: code,
-    };
-  }
+  return {
+    skipped: !code,
+    changed: code !== contents,
+    source: code,
+  };
+}
 
 describe('app', function() {
-    it('should do the thing', function(done) {
-        const pathName = path.resolve(__dirname, 'components/**/*.hbs');
-        globby(pathName, { absolute: true })
-            .then(files => {
-                if (files.length < 1) {
-                throw new Error('No files found');
-                }
+  it('should do the thing', function(done) {
+    const pathName = path.resolve(__dirname, 'components/**/*.hbs');
+    globby(pathName, { absolute: true })
+      .then(files => {
+        if (files.length < 1) {
+          throw new Error('No files found');
+        }
             
-                return files;
-            })
-            .then((files) => {
-                const componentNames = files.map((file) => file.split('components/')[1].replace('.hbs', ''));
-                componentNames.map((name) => componentsReigistry[name] = []);
-                const promiseArray = files.map((fileName) => 
-                    readFile(fileName)
-                        .then((contents) => applyTransform(findDeps, fileName, contents))
-                );
-                return Promise.all(promiseArray).then((args) => {
-                    let resolvedDeps = [];
-                    // Add the things with 1 dep first then build up from there
-                    let index = 1;
-                    while (resolvedDeps.length != Object.keys(componentsReigistry).length) {
-                        const deps = Object.keys(componentsReigistry).filter((key) => componentsReigistry[key].length === index);
-                        for (const dep of deps) {
-                            Object.keys(componentsReigistry).forEach((key) => {
-                                if (componentsReigistry[key].includes(dep)) {
-                                    // Push all of this components deps to the consuming components dependencies
-                                    pushOrIgnore(componentsReigistry[key], componentsReigistry[dep]);
-                                }
-                            });
-                        }
-                        index++;
-                        resolvedDeps.push(...deps);
-                    }
-                return componentsReigistry;
-                })
-            })
-            .then((deps) => {
-                console.log(deps);
-                assert.deepEqual(deps['test'], []);
-                assert.deepEqual(deps['test2'], [ 'test' ]);
-                assert.deepEqual(deps['test3'], [ 'test2', 'nested/nested', 'test' ]);
-                assert.deepEqual(deps['test4'], [ 'test3', 'test2', 'nested/nested', 'test' ]);
-                assert.deepEqual(deps['nested/nested'], [ 'tegst' ]);
-                done();
-            });
-    });
+        return files;
+      })
+      .then((files) => {
+        const componentNames = files.map((file) => file.split('components/')[1].replace('.hbs', ''));
+        componentNames.map((name) => componentsReigistry[name] = []);
+        const promiseArray = files.map((fileName) => 
+          readFile(fileName)
+            .then((contents) => applyTransform(findDeps, fileName, contents))
+        );
+        return Promise.all(promiseArray).then(() => {
+          let resolvedDeps = [];
+          // Add the things with 1 dep first then build up from there
+          let index = 1;
+          while (resolvedDeps.length != Object.keys(componentsReigistry).length) {
+            const deps = Object
+              .keys(componentsReigistry)
+              .filter((key) => componentsReigistry[key].length === index);
+
+            for (const dep of deps) {
+              Object.keys(componentsReigistry)
+                .forEach((key) => {
+                  if (componentsReigistry[key].includes(dep)) {
+                  // Push all of this components deps to the consuming components dependencies
+                    pushOrIgnore(componentsReigistry[key], componentsReigistry[dep]);
+                  }
+                });
+            }
+            index++;
+            resolvedDeps.push(...deps);
+          }
+          return componentsReigistry;
+        })
+      })
+      .then((deps) => {
+        assert.deepStrictEqual(deps['test'], []);
+        assert.deepStrictEqual(deps['test2'], [ 'test' ]);
+        assert.deepStrictEqual(deps['test3'], [ 'test2', 'nested/nested', 'test' ]);
+        assert.deepStrictEqual(deps['test4'], [ 'test3', 'test2', 'nested/nested', 'test' ]);
+        assert.deepStrictEqual(deps['nested/nested'], [ 'test' ]);
+        done();
+      });
+  });
 });
 
